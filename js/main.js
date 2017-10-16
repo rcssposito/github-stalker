744f1307f4eb0db6b48a6d125fad80ee24cf0547
rcssposito <rcssposito@gmail.com>
2017-10-16 01:11:07 -0200

    Perdeu a graça já

--- js/main.js
+++ js/main.js
@@ -21,41 +21,60 @@ $.ajax({
     data:{
         client_id:'1dc65264b4cd3aada7ee',
         client_secret:'891c7f6a077a55aa76f80c8ec09b94d908fe967c',
+        per_page: 50
     }
         
 })
     //exibir array com repositórios que o usuário deu star 
 .done(function(repos){
+ 
+    $('#container').html(`
+<img class="picture" src="${user.avatar_url}"></img>
+<header id="profile">${user.name}</header>
+<div id="repos"></div>
+`)
+    
+    $.map(repos, function(repo, index){ //função para receber os dados do array e gravar no html
+               
+     $("#repos").append(`
 
+<ul id="cards">
+<div class="infos">
+<li ><label class="label">Name: ${repo.name}</label></li>
+<li ><label class="label">Owner/Repo: ${repo.full_name}</label></li>
+<li ><label class="label">Stars: ${repo.stargazers_count}</label></li>
+<li ><label class="label">Linguagem ${repo.language}</label></li>
+<li ><label class="label">Open Issues ${repo.open_issues_count}</label></li>
+<li ><a href="${repo.html_url}" target="parent"><button>Repo URL </button><a/></li>
+</div>
+</ul>
+`)})
+   
     $("input[name=filter]").change(function (){//recer o parâmetro "value" dos radios
         var radios = ($(this).attr("value"));
 
-    if(radios =="name"){//provávelmente essa não é a melhor foram de fazer isso, mas foi a unica que der certo
-
-        /*eu compreendo que json é nada mais que um array, logo, eu crio um método sort para ordenar ele, mas, o grande problema aqui
-        é que eu não consigo setar o valor de um radio na variável e ler ela nesse método sort. Se eu conseguir isso, da pra reduzir 
-        esse código */
+    if(radios =="name"){
         
         repos.sort(function(a,b) {
-        return a.name < b.name ? 1 : a.name > b.name ? -1 : 0;
+        return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
   })
     }
     if(radios =="stargazers_count"){
 
         repos.sort(function(a,b) {
-        return a.stargazers_count < b.stargazers_count ? 1 : a.stargazers_count > b.stargazers_count ? -1 : 0;
+        return a.stargazers_count < b.stargazers_count ? -1 : a.stargazers_count > b.stargazers_count ? 1 : 0;
   })     
     }
     if(radios =="open_issues_count"){
 
         repos.sort(function(a,b) {
-        return a.open_issues_count < b.open_issues_count ? 1 : a.open_issues_count > b.open_issues_count ? -1 : 0;
+        return a.open_issues_count < b.open_issues_count ? -1 : a.open_issues_count > b.open_issues_count ? 1 : 0;
   })
     }
         
      $("#repos").empty() //método para apagar os dados obtidos préviamente
     
-        $.each(repos, function(index, repo){ //função para receber os dados do array e gravar no html
+        $.map(repos, function(repo, index){ //função para receber os dados do array e gravar no html
                
             $("#repos").append(`
 
@@ -70,16 +89,11 @@ $.ajax({
 </div>
 </ul>
 `)
+            
+        
     })//fim da função each
     })//fim da função para receber os métodos do array       
-});//fim da função repo 
-    
-    $('#container').html(`
-<img class="picture" src="${user.avatar_url}"></img>
-<header id="profile">${user.name}</header>
-<div id="repos"></div>
-`)
-    
+})//fim da função repo 
     
   });//fim da função user        
  });//fim da função e
